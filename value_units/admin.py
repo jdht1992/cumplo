35c52fc85bb0ce7f753282320ed36c80d544933f
Diego Herrera <jdht1992@gmail.com>
2020-11-12 13:20:02 -0600

    create model currency

--- value_units/admin.py
+++ value_units/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Currency
 
-# Register your models here.
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'value', 'kind')
+
+
+admin.site.register(Currency, CurrencyAdmin)
--- value_units/models.py
+++ value_units/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+KIND = (
+        (1, 'UDI'),
+        (2, 'DOLAR'),
+    )
+
+class Currency(models.Model):
+    date = models.DateField()
+    value = models.DecimalField(max_digits=10, decimal_places=6)
+    kind = models.PositiveSmallIntegerField(choices=KIND, default=1)
+
+    def __str__(self):
+        return f"{self.kind}"
